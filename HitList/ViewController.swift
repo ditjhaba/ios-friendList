d3d8236d8cf7d90bd1b1578a02f45395484ecad0
Ditjhaba Selemela <edselemela@gmail.com>
2015-11-16 13:49:26 +0200

    Initial commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Ditjhaba Selemela on 2015/11/16.
+//  Copyright © 2015 Ditjhaba Selemela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    @IBAction func addName(sender: AnyObject) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
