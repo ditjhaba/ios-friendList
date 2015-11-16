a11c015fa3c92887480f1eb61140fbd995e0c195
Ditjhaba Selemela <edselemela@gmail.com>
2015-11-16 14:02:38 +0200

    Implementation for UITableViewDataSource protocol functions

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -8,17 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
-    
+    var names = [String]()
 
     @IBAction func addName(sender: AnyObject) {
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "\"The List\""
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+        
+        cell!.textLabel?.text = names[indexPath.row]
+        return cell!
     }
 
     override func didReceiveMemoryWarning() {
